b3e74bf00282515330ad5e64694e343bd518edb8
Rafael-Romeu <rafaelromeufurg@gmail.com>
2018-07-04 21:28:49 -0300

    Atualiza CarregaConsulta.php

--- serverScripts/CarregaConsulta.php
+++ serverScripts/CarregaConsulta.php
@@ -5,58 +5,69 @@
     include_once "../logicas/lAtendente.php";
 
 
-
-
     $oConsulta = new lConsulta();
     $oMedico = new lMedico();
     $oPaciente = new lPaciente();
     $oAtendente = new lAtendente();    
 
 
+    $codigo = $_REQUEST["Codigo"]; 
+    $Medico = $_REQUEST["medico"];
+
     //Se Atendente
-    //Filtra as Consultas a partir do medico escolhido
-    $codigo = $_REQUEST["codigo"]; 
-    $Medico = $_REQUEST["Medico"];
-
-    echo "<tr>";
-    echo    "<td>Medico</td>";
-    echo    "<td>Paciente</td>";
-    echo    "<td>Atendente</td>";
-    echo    "<td>Data</td>";
-    echo    "<td>Hora</td>";
-    echo  "</tr>";
-
-    $ListaConsultasXML = $oConsulta->selectConsulta(null,null,$Medico);
-    $ListaIConsultas = $oConsulta->traduzSimpleXMLObjectToConsulta($ListaConsultasXML);
-    
-    foreach ($ListaIConsultas as $Consulta)
+    if (substr($codigo, 0, 1) == "A")
     {
-        $codigoMedico = $Consulta->codMedico;
-        $ListaMedicos = $oMedico->getMedicoByCodigo($codigoMedico);
-        $codigoPaciente = $Consulta->codPaciente;
-        $ListaPacientes = $oPaciente->getPacienteByCodigo($codigoPaciente);
-        $codigoAtendente = $Consulta->codAtendente;
-        $ListaAtendentes = $oAtendente->getAtendenteByCodigo($codigoAtendente);
-            
-        foreach ($ListaMedicos as $oMed)
-        {
-            $nomeMedico = $oMed->nome;
-        }
-        foreach ($ListaPacientes as $oPac)
-        {
-            $nomePaciente = $oPac->nome;
-        }
-        foreach ($ListaAtendentes as $oAtend)
-        {
-            $nomeAtendente = $oAtend->nome;
-        }
+        //Filtra as Consultas a partir do medico escolhido
         echo "<tr>";
-        echo    "<td>".$nomeMedico."</td>";
-        echo    "<td>".$nomePaciente."</td>";
-        echo    "<td>".$nomeAtendente."</td>";
-        echo    "<td>".$Consulta->data."</td>";
-        echo    "<td>".$Consulta->hora."</td>";
+        echo    "<td>Medico</td>";
+        echo    "<td>Paciente</td>";
+        echo    "<td>Atendente</td>";
+        echo    "<td>Data</td>";
+        echo    "<td>Hora</td>";
         echo  "</tr>";
+
+        $ListaConsultasXML = $oConsulta->selectConsulta(null,null,$Medico);
+        $ListaIConsultas = $oConsulta->traduzSimpleXMLObjectToConsulta($ListaConsultasXML);
+        
+        foreach ($ListaIConsultas as $Consulta)
+        {
+            $codigoMedico = $Consulta->codMedico;
+            $ListaMedicos = $oMedico->getMedicoByCodigo($codigoMedico);
+            $codigoPaciente = $Consulta->codPaciente;
+            $ListaPacientes = $oPaciente->getPacienteByCodigo($codigoPaciente);
+            $codigoAtendente = $Consulta->codAtendente;
+            $ListaAtendentes = $oAtendente->getAtendenteByCodigo($codigoAtendente);
+                
+            foreach ($ListaMedicos as $oMed)
+            {
+                $nomeMedico = $oMed->nome;
+            }
+            foreach ($ListaPacientes as $oPac)
+            {
+                $nomePaciente = $oPac->nome;
+            }
+            foreach ($ListaAtendentes as $oAtend)
+            {
+                $nomeAtendente = $oAtend->nome;
+            }
+            echo "<tr>";
+            echo    "<td>".$nomeMedico."</td>";
+            echo    "<td>".$nomePaciente."</td>";
+            echo    "<td>".$nomeAtendente."</td>";
+            echo    "<td>".$Consulta->data."</td>";
+            echo    "<td>".$Consulta->hora."</td>";
+            echo  "</tr>";
+        }
+    }
+    //Se Medico
+    elseif(substr($codigo, 0, 1) == "M")
+    {
+
+    }
+    //Se Paciente
+    elseif(substr($codigo, 0, 1) == "P")
+    {
+
     }
 
 ?> 
