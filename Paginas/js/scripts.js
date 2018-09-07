1136bf43623093e9c853e84b25ffe69a86c94692
Rafael Bulsing <rafabulsing@gmail.com>
2018-09-07 14:33:42 -0300

    Implement BEM css

---
+++ Paginas/js/scripts.js
@@ -0,0 +1,74 @@
+function SvgInliner() {
+    document.querySelectorAll('img.svg').forEach(function(img){
+        var imgID = img.id;
+        var imgClass = img.className;
+        var imgURL = img.src;
+
+        fetch(imgURL).then(function(response) {
+            return response.text();
+        }).then(function(text){
+
+            var parser = new DOMParser();
+            var xmlDoc = parser.parseFromString(text, "text/xml");
+
+            // Get the SVG tag, ignore the rest
+            var svg = xmlDoc.getElementsByTagName('svg')[0];
+
+            // Add replaced image's ID to the new SVG
+            if(typeof imgID !== 'undefined') {
+                svg.setAttribute('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if(typeof imgClass !== 'undefined') {
+            svg.setAttribute('class', imgClass+' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            svg.removeAttribute('xmlns:a');
+
+            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+            if(!svg.getAttribute('viewBox') && svg.getAttribute('height') && svg.getAttribute('width')) {
+                svg.setAttribute('viewBox', '0 0 ' + svg.getAttribute('height') + ' ' + svg.getAttribute('width'))
+            }
+
+            // Replace image with new SVG
+            img.parentNode.replaceChild(svg, img);
+
+        });
+
+    });
+}
+
+function ConsultasFilter() {
+    $( "#filter-toggle" ).click(function() {
+        this.classList.toggle("consultas-widget__filter-icon--active");
+        $( "#filter-box" ).toggle( 500 , "swing" );
+    });
+}
+
+function ConsultasAccordion() {
+    var acc = document.getElementsByClassName("consultas-widget__list-row");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            var j;
+            for (j = 0; j < acc.length; j++) {
+                if (acc[j] != this)
+                {
+                acc[j].classList.remove("consultas-widget__list-row--active");
+                acc[j].lastElementChild.style.maxHeight = null;
+                }
+            }
+            this.classList.toggle("consultas-widget__list-row--active");
+            var panel = this.lastElementChild;
+            if (panel.style.maxHeight){
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            } 
+        });
+    }
+}
+
+
