d96cd110b4eeb16432dc5c65576ebe9f10998f55
Marlon Franco <marlonrcfranco@gmail.com>
2018-09-09 06:30:50 -0300

    Adicionado INIT.sql para criar e popular base de dados

---
+++ BancoDeDados/MySQL/Scripts/fontes/INSERT.sql
@@ -0,0 +1,14 @@
+####### INSERCOES #######
+
+####### tAtendente #######
+
+INSERT INTO `tAtendente` (`codigo`, `nome`, `senha`, `cpf`, `dataNascimento`, `endereco`, `CEP`, `telefone1`, `telefone2`, `email`, `regDate`) VALUES (NULL, 'Marlon Rubio de Carvalho Franco', '1234567', '77777777777', '1993-05-31', 'Rua da minha casa, 333, apto 201', '77777777', '+55 53 7787 7777', '', 'marlon@scadiagro.com.br', CURRENT_TIMESTAMP)
+
+
+
+
+
+
+
+
+
