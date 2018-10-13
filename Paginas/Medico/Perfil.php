baf4a3ac4c4293600ed8db763b795fe6ddc79f68
Rafael Bulsing <rafabulsing@gmail.com>
2018-10-12 22:47:35 -0300

    Adiciona nomes as paginas do Medico

--- Paginas/Medico/Perfil.php
+++ Paginas/Medico/Perfil.php
@@ -5,7 +5,7 @@
         header("location: /Paginas/Login.php");
         exit;
     }
-    if($_SESSION['tipo'] != "lPaciente"){
+    if($_SESSION['tipo'] != "lMedico"){
         shell_exec('php ' . $_SERVER['DOCUMENT_ROOT'] . '/ServerScripts/Logout.php');
         header('location: /Paginas/Login.php');
         exit;
@@ -34,7 +34,7 @@
     <div class="main-header__top-bar">
       <h1 class="main-header__logo">Vida Saudável</h1>
       <div class="main-header__user">
-        <span class="main-header__username" id="headerUserNome">Jacinto Leite</span>
+        <span class="main-header__username" id="headerUserNome"><?php echo htmlspecialchars($_SESSION['nome']); ?></span>
         <a class="main-header__logout-btn" href="#">Logout</a>
       </div>
     </div>
