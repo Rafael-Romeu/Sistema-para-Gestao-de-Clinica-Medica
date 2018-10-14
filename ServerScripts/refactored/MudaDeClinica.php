436abe51267ad0a3d17d5ebbbfd14d90292b797d
Rafael Bulsing <rafabulsing@gmail.com>
2018-10-14 14:25:05 -0300

    Atualiza as cores em Session quando muda de clinica

--- ServerScripts/refactored/MudaDeClinica.php
+++ ServerScripts/refactored/MudaDeClinica.php
@@ -14,4 +14,13 @@
 	$_SESSION['codClinica'] = $codigo;
 	$_SESSION['nomeClinica'] = $nome;
 
+	$_SESSION['corPrimaria'] = $clinica->getCorPrimaria();
+	$_SESSION['corSucesso'] = $clinica->getCorSucesso();
+	$_SESSION['corFalha'] = $clinica->getCorFalha();
+	$_SESSION['cor1'] = $clinica->getCor1();
+	$_SESSION['cor2'] = $clinica->getCor2();
+	$_SESSION['cor3'] = $clinica->getCor3();
+	$_SESSION['cor4'] = $clinica->getCor4();
+	$_SESSION['cor5'] = $clinica->getCor5();
+
 ?>
